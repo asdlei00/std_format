d268c948ccf0f42516f931bed9e88ef20d3c2d81
asdlei <asdlei99@163.com>
2015-05-14 10:54:42 +0800

    stand format
    a std-format sample

---
+++ std_format.h
@@ -0,0 +1,23 @@
+
+/**************************************************************************************************
+/* Copyright (C) @leilei, BY LILEI, 2015
+/*
+/*  FILE NAME             :  common.h
+/*  PRINCIPAL AUTHOR      :  lilei
+/*  SUBSYSTEM NAME        :  STL
+/*  MODULE NAME           :
+/*  LANGUAGE              :  C++
+/*  TARGET ENVIRONMENT    :  ANY
+/*  DATE OF FIRST RELEASE :  2015/05/14
+/*  DESCRIPTION           :  This is a "common" header file
+/**************************************************************************************************
+
+/*
+ * Revision log:
+ * 1.
+ * Version: 1.0
+ * Author:  Lilei
+ * Time:    2015/05/14
+ * Content: Created
+ *
+ */
